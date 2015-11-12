d404545b6ebff5c726249b9276cf9ca98e8deecf
Howard Chao <zollee7817@gmail.com>
2015-11-12 17:03:02 +0800

    添加值班部长条

---
+++ version_2/签到组.jsx
@@ -0,0 +1,555 @@
+﻿.签到组 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 60;
+}
+.开始值班 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 59;
+}
+.姓名： {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 723px;
+  top: 200.766px;
+  z-index: 58;
+}
+.当前时间：2015_11_05_15:07:00_星期四 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(22, 22, 22);
+  line-height: 1.2;
+  position: absolute;
+  left: 222px;
+  top: 200.766px;
+  z-index: 57;
+}
+.输入框 {
+  background-color: rgb(33, 150, 243);
+  position: absolute;
+  left: 783px;
+  top: 223px;
+  width: 160px;
+  height: 2px;
+  z-index: 56;
+}
+.输入姓氏可自动补全 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(205, 205, 205);
+  line-height: 1.2;
+  position: absolute;
+  left: 781px;
+  top: 201.766px;
+  z-index: 55;
+}
+.开始值班 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(255, 255, 255);
+  line-height: 1.2;
+  position: absolute;
+  left: 1050px;
+  top: 199.766px;
+  z-index: 54;
+}
+.按钮背景 {
+  background-color: rgb(3, 169, 244);
+  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.35);
+  position: absolute;
+  left: 1026px;
+  top: 194px;
+  width: 120px;
+  height: 36px;
+  z-index: 53;
+}
+.背景 {
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  left: 183px;
+  top: 180px;
+  width: 1000px;
+  height: 64px;
+  z-index: 52;
+}
+.值班部长 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 50;
+}
+.值班部长： {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 223px;
+  top: 282.766px;
+  z-index: 49;
+}
+.复选组 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 48;
+}
+.复选框3 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 743px;
+  top: 322px;
+  width: 16px;
+  height: 16px;
+  z-index: 47;
+}
+.你好啊3 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -webkit-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -ms-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  position: absolute;
+  left: 778.5px;
+  top: 320.266px;
+  z-index: 46;
+}
+.复选框2 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 535px;
+  top: 321px;
+  width: 16px;
+  height: 16px;
+  z-index: 45;
+}
+.你好啊2 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00585781050279,0,0,0.99404894740757,0,0);
+  -webkit-transform: matrix( 1.00585781050279,0,0,0.99404894740757,0,0);
+  -ms-transform: matrix( 1.00585781050279,0,0,0.99404894740757,0,0);
+  position: absolute;
+  left: 570.5px;
+  top: 318.266px;
+  z-index: 44;
+}
+.复选框1 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 327px;
+  top: 320px;
+  width: 15px;
+  height: 16px;
+  z-index: 43;
+}
+.你好啊1 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -webkit-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -ms-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  position: absolute;
+  left: 362px;
+  top: 318.266px;
+  z-index: 42;
+}
+.结束值班 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(255, 255, 255);
+  line-height: 1.2;
+  position: absolute;
+  left: 1050.25px;
+  top: 317.766px;
+  z-index: 40;
+}
+.按钮背景 {
+  background-color: rgb(3, 169, 244);
+  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.35);
+  position: absolute;
+  left: 1026px;
+  top: 312px;
+  width: 120px;
+  height: 36px;
+  z-index: 39;
+}
+.背景 {
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  left: 183px;
+  top: 264px;
+  width: 1000px;
+  height: 100px;
+  z-index: 38;
+}
+.正在值班 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 36;
+}
+.正在值班： {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 223px;
+  top: 402.766px;
+  z-index: 35;
+}
+.复选组 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 34;
+}
+.框1_1 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 327px;
+  top: 440px;
+  width: 15px;
+  height: 16px;
+  z-index: 33;
+}
+.框1_2 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 327px;
+  top: 470px;
+  width: 15px;
+  height: 16px;
+  z-index: 32;
+}
+.框1_3 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 327px;
+  top: 501px;
+  width: 15px;
+  height: 16px;
+  z-index: 31;
+}
+.框2_1 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 535px;
+  top: 441px;
+  width: 16px;
+  height: 16px;
+  z-index: 30;
+}
+.框2_2 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 535px;
+  top: 471px;
+  width: 16px;
+  height: 16px;
+  z-index: 29;
+}
+.框2_3 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 535px;
+  top: 502px;
+  width: 16px;
+  height: 16px;
+  z-index: 28;
+}
+.框3_1 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 743px;
+  top: 442px;
+  width: 16px;
+  height: 16px;
+  z-index: 27;
+}
+.框3_2 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 743px;
+  top: 472px;
+  width: 16px;
+  height: 16px;
+  z-index: 26;
+}
+.框3_3 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 743px;
+  top: 503px;
+  width: 16px;
+  height: 16px;
+  z-index: 25;
+}
+.框4_1 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 952px;
+  top: 442px;
+  width: 16px;
+  height: 16px;
+  z-index: 24;
+}
+.框4_2 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 952px;
+  top: 472px;
+  width: 16px;
+  height: 16px;
+  z-index: 23;
+}
+.框4_3 {
+  border-width: 1px;
+  border-color: rgb(0, 4, 11);
+  border-style: dashed;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 952px;
+  top: 503px;
+  width: 16px;
+  height: 16px;
+  z-index: 22;
+}
+.你好啊1 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -webkit-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -ms-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  position: absolute;
+  left: 362px;
+  top: 438.266px;
+  z-index: 21;
+}
+.你好啊_2 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00585781050279,0,0,0.99404894740757,0,0);
+  -webkit-transform: matrix( 1.00585781050279,0,0,0.99404894740757,0,0);
+  -ms-transform: matrix( 1.00585781050279,0,0,0.99404894740757,0,0);
+  position: absolute;
+  left: 570.5px;
+  top: 438.266px;
+  z-index: 20;
+}
+.你好啊3 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -webkit-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  -ms-transform: matrix( 1.00078554595444,0,0,1.00022316447222,0,0);
+  position: absolute;
+  left: 778.5px;
+  top: 440.266px;
+  z-index: 19;
+}
+.你好啊4 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  -moz-transform: matrix( 1.00578590200132,0,0,1.00022316447222,0,0);
+  -webkit-transform: matrix( 1.00578590200132,0,0,1.00022316447222,0,0);
+  -ms-transform: matrix( 1.00578590200132,0,0,1.00022316447222,0,0);
+  position: absolute;
+  left: 987.5px;
+  top: 441.266px;
+  z-index: 18;
+}
+.结束值班 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(255, 255, 255);
+  line-height: 1.2;
+  position: absolute;
+  left: 1050.25px;
+  top: 543.766px;
+  z-index: 16;
+}
+.按钮背景 {
+  background-color: rgb(3, 169, 244);
+  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.35);
+  position: absolute;
+  left: 1026px;
+  top: 538px;
+  width: 120px;
+  height: 36px;
+  z-index: 15;
+}
+.背景 {
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  left: 183px;
+  top: 384px;
+  width: 1000px;
+  height: 200px;
+  z-index: 14;
+}
+.值班记录 {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1366px;
+  height: 1068px;
+  z-index: 12;
+}
+.最近值班： {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 233px;
+  top: 617.766px;
+  z-index: 11;
+}
+.所在部门 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 323px;
+  top: 652.766px;
+  z-index: 10;
+}
+.姓名 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 567px;
+  top: 652.766px;
+  z-index: 9;
+}
+.开始时间 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 759px;
+  top: 652.766px;
+  z-index: 8;
+}
+.结束时间 {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 961.25px;
+  top: 652.766px;
+  z-index: 7;
+}
+.横线 {
+  background-color: rgb(224, 224, 224);
+  position: absolute;
+  left: 308px;
+  top: 678px;
+  width: 751px;
+  height: 1px;
+  z-index: 6;
+}
+.运行三部_______________________________李晨阳_________ {
+  font-size: 18px;
+  font-family: "Microsoft YaHei UI";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  position: absolute;
+  left: 323px;
+  top: 688.766px;
+  z-index: 5;
+}
+.背景 {
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  left: 183px;
+  top: 604px;
+  width: 1000px;
+  height: 410px;
+  z-index: 4;
+}
